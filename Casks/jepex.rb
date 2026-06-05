9d7792d2bf19dcced1c205a6c0f6383340cfdfdc
Jekaterina Berkovica <36275311+jberkovica@users.noreply.github.com>
2026-06-05 12:59:57 +0000

    jepex 1.2.0

--- Casks/jepex.rb
+++ Casks/jepex.rb
@@ -1,6 +1,6 @@
 cask "jepex" do
-  version "1.0.0"
-  sha256 "2f99e011788b178e42e1f527141900ebb07859e999abf1793f5f391bf34f13c3"
+  version "1.2.0"
+  sha256 "3620a01538d4a2886f0ea35ff0fb3b54390d87a97c6108c32353e5f0b7ffa49e"
 
   url "https://downloads.jepex.app/macos/jepex-#{version}.dmg"
   name "jepex"
